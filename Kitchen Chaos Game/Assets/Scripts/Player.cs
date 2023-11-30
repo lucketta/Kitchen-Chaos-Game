4bfeaeeb7ced8c178d1fbd231304e8e9d523ad99
Anna <lucketta@umich.edu>
2023-11-30 07:23:15 -0800

    Update Player.cs to lets player turn when up against counter

--- Kitchen Chaos Game/Assets/Scripts/Player.cs
+++ Kitchen Chaos Game/Assets/Scripts/Player.cs
@@ -94,7 +94,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 
             // Attempt only X Movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if (canMove) {
                 // Move only on the X
@@ -105,7 +105,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
 
                 // Attempt only Z Movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove) {
                     // Move only on the Z
